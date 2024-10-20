12c5f3e2819fc9ef1199c134c12cb35ccdfcc153
handlename <nagata@handlena.me>
2024-10-20 12:51:37 +0900

    feat: dump errors

--- cli/cli.go
+++ cli/cli.go
@@ -3,10 +3,12 @@ package cli
 import (
 	"context"
 	"errors"
+	"fmt"
 	"os"
 	"os/signal"
 
 	myapp "github.com/handlename/my-golang-template"
+	"github.com/morikuni/failure/v2"
 	"github.com/rs/zerolog/log"
 )
 
@@ -44,11 +46,30 @@ func Run() ExitCode {
 		if errors.Is(err, context.Canceled) {
 			log.Error().Msg("canceled")
 		} else {
-			log.Error().Stack().Err(err).Msg("")
+			handleError(err)
 		}
 
 		return ExitCodeError
 	}
 
 	return ExitCodeOK
+}
+
+func handleError(err error) {
+	fmt.Println("======== error ========")
+
+	code := failure.CodeOf(err)
+	fmt.Printf("code = %s\n", code)
+
+	msg := failure.MessageOf(err)
+	fmt.Printf("message = %s\n", msg)
+
+	cs := failure.CallStackOf(err)
+	fmt.Printf("callstack = %s\n", cs)
+
+	fmt.Printf("cause = %s\n", failure.CauseOf(err))
+
+	fmt.Println()
+	fmt.Println("======== detail ========")
+	fmt.Printf("%+v\n", err)
 }
