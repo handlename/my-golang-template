4f9ee82c9aa493e25e0daa75fbeb4047a1b4c324
handlename <nagata@handlena.me>
2024-10-20 12:51:25 +0900

    feat: add error codes

--- internal/errorcode/errorcode.go
+++ internal/errorcode/errorcode.go
@@ -3,5 +3,7 @@ package errorcode
 type ErrorCode string
 
 const (
-	ErrNotImplemented ErrorCode = "NotImplemented"
+	ErrNotImplemented  ErrorCode = "NotImplemented"
+	ErrInternal        ErrorCode = "Internal"
+	ErrInvalidArgument ErrorCode = "InvalidArgument"
 )
