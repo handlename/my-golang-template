2cec94bb05f7366e5e84180d83bfe41bbd0816b2
NAGATA Hiroaki <nagata@handlena.me>
2025-03-21 23:15:22 +0900

    feat: run myapp.Run

--- cli/command/root.go
+++ cli/command/root.go
@@ -6,3 +6,7 @@ type Root struct {
 	LogLevel string           `help:"Set log level. (trace|debug|info|warn|error|panic)" default:"info"`
 	Version  kong.VersionFlag `help:"Show version."`
 }
+
+func (r *Root) Run(c *Context) error {
+	return c.App.Run(c.Ctx)
+}
